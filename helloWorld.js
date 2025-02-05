8a2f5b9d6d7e611d117a728961f6ce37344c8198
Busdan_Joshua <revildobusdan@gmail.com>
2025-02-05 22:11:39 +0700

    adding exercise2

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+const hello = "Hello World ";
+
+const greetings = () => `Hi, my name is John`;
+
+module.exports = { hello, greetings };
---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const moment = require("moment");
+const users = require("./users");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write("This is the home page");
+  } else if (url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write(
+      JSON.stringify({
+        status: "success",
+        message: "response success",
+        description: "exercise #02",
+        date: moment().format(" MMMM Do YYYY, h:mm:ss a"),
+      })
+    );
+  } else if (url === "/users") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write(JSON.stringify(users));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/json");
+    res.write("404 Users Not Found");
+  }
+
+  res.end();
+});
+
+const hostname = "127.0.0.1";
+const port = 3000;
+server.listen(port, hostname, () => {
+  console.log(`server running at http://${hostname}:${port}`);
+});
